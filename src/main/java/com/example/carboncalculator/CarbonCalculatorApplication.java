114bc1df7e25582be177a8f1c9beee9adc4aaa15
Yaswanth-59 <yaswanthk743@gmail.com>
2026-07-16 19:37:41 +0530

    Update CarbonCalculatorApplication.java

--- src/main/java/com/example/carboncalculator/CarbonCalculatorApplication.java
+++ src/main/java/com/example/carboncalculator/CarbonCalculatorApplication.java
@@ -2,8 +2,12 @@ package com.example.carboncalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.carboncalculator.repository")
+@EntityScan(basePackages = "com.example.carboncalculator.model")
 public class CarbonCalculatorApplication {
 
     public static void main(String[] args) {
