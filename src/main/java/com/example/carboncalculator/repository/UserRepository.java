eabdae50c8a7cfe57551b5583353c78398b9c8b3
Yaswanth-59 <yaswanthk743@gmail.com>
2026-07-16 20:04:23 +0530

    Update UserRepository.java

--- src/main/java/com/example/carboncalculator/repository/UserRepository.java
+++ src/main/java/com/example/carboncalculator/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.example.carboncalculator.repository;
 
 import com.example.carboncalculator.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByEmail(String email);
+
 }
